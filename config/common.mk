9ccd0740ac6f5ab5f14d588c7d8b329bbf9e9a72
nift4 <nift4@protonmail.com>
2023-04-14 00:37:39 +0200

    vendor: Remove useless addon.d file
    * Avoid detection as custom ROM
    
    Change-Id: I1fbd63cc116d53c7c366662b632163f6bc079bbf

--- config/common.mk
+++ config/common.mk
@@ -27,11 +27,7 @@ endif
 # Backup Tool
 PRODUCT_COPY_FILES += \
     vendor/minus/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
-    vendor/minus/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
-    vendor/minus/prebuilt/common/bin/50-minus.sh:$(TARGET_COPY_OUT_SYSTEM)/addon.d/50-minus.sh
-
-PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
-    system/addon.d/50-minus.sh
+    vendor/minus/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions
 
 ifneq ($(strip $(AB_OTA_PARTITIONS) $(AB_OTA_POSTINSTALL_CONFIG)),)
 PRODUCT_COPY_FILES += \
