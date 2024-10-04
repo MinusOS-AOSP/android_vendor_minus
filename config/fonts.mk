efdb569b3d386d603526c05764478892b37f7287
RITEFANG <1721985272@qq.com>
2024-10-04 17:04:48 +0800

    vendor: Set MISans Text as the default body font.
    * With Android 12, Google Sans is now a variable font type as in
      the font (.ttf) file now contains various different font glyphs
      that the system can use when it requests for a particular weight
      and OPSZ axis values. Google Sans Text - Google's new Roboto body
      font companion is also bundled in this font file.
    
    * To make use of the different glyphs stored in the font file we
      define the OPSZ and weight of the fonts to inline with what Pixels
      have thus, giving us the same look and feel.
    
    * Change the framework font overlay to reflect the new font alias we
      created in `font_customization.xml`
    
    Change-Id: I48a747c88a23e1b8c8e20b762a6f403b8d6c4637
    Reviewed-on: https://review.statixos.com/c/android_vendor_statix/+/5749
    Reviewed-by: Sourajit Karmakar <sourajit@live.com>
    Tested-by: Sourajit Karmakar <sourajit@live.com>
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>
    Signed-off-by: RITEFANG <1721985272@qq.com>

--- config/fonts.mk
+++ config/fonts.mk
@@ -4,5 +4,7 @@ PRODUCT_COPY_FILES += \
     vendor/minus/fonts/MiSans-Light.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/MiSans-Light.ttf \
     vendor/minus/fonts/MiSans-Normal.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/MiSans-Normal.ttf \
     vendor/minus/fonts/MiSans-Bold.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/MiSans-Bold.ttf \
-    vendor/minus/fonts/MiSans-Heavy.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/MiSans-Heavy.ttf \
+    vendor/minus/fonts/MiSans-Heavy.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/MiSans-Heavy.ttf \    
+    vendor/minus/fonts/GoogleSans-Italic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Italic.ttf \
     vendor/minus/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
+
